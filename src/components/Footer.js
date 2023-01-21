8597a7c5ef3516e84d723ddc19a91b37af1ab96e
Jordan Ulmer <julmer526@gmail.com>
2023-01-21 16:54:34 +0000

    Home component started and Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+
+    return (
+        <div className="footerContainer">
+            <div>Copyright Jordan Ulmer 2023</div>
+            <div className="github">github</div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Home = () => {
+    
+    return (
+        <div className="homeContainer">
+
+            <div className="tagline">Elevate your space with AFD Access Flooring.</div>
+
+            <div className="shopNowContainer">
+                <button className="shopNowBtn">Shop Now</button>
+            </div>
+
+            <div className="homeImageContainer">
+                <img alt="Terrazzo Flooring" src="images/AFDHome.jpg"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Home
